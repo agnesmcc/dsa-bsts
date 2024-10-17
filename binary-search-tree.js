a554ebf3b7694a0bbcf9d898f70bee91d8497237
Agnes <agnes.mccall5@gmail.com>
2024-10-16 21:57:14 -0400

    add remove node method

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -186,7 +186,83 @@ class BinarySearchTree {
    * Returns the removed node. */
 
   remove(val) {
-
+    let current = this.root;
+    let parent = null;
+  
+    // Traverse down the tree until we find the node to be removed
+    while (current) {
+      if (val === current.val) {
+        // Node found, now handle removal
+        // If the node has no children, we can simply remove it
+        if (!current.left && !current.right) {
+          if (!parent) {
+            // If the node is the root, we simply set the root to null
+            this.root = null;
+          } else if (current.val < parent.val) {
+            // If the node is the left child of the parent, we set the left child to null
+            parent.left = null;
+          } else {
+            // If the node is the right child of the parent, we set the right child to null
+            parent.right = null;
+          }
+        } else if (!current.left) {
+          // If the node only has a right child, we replace the node with its right child
+          if (!parent) {
+            // If the node is the root, we set the root to the right child
+            this.root = current.right;
+          } else if (current.val < parent.val) {
+            // If the node is the left child of the parent, we set the left child to the right child
+            parent.left = current.right;
+          } else {
+            // If the node is the right child of the parent, we set the right child to the right child
+            parent.right = current.right;
+          }
+        } else if (!current.right) {
+          // If the node only has a left child, we replace the node with its left child
+          if (!parent) {
+            // If the node is the root, we set the root to the left child
+            this.root = current.left;
+          } else if (current.val < parent.val) {
+            // If the node is the left child of the parent, we set the left child to the left child
+            parent.left = current.left;
+          } else {
+            // If the node is the right child of the parent, we set the right child to the left child
+            parent.right = current.left;
+          }
+        } else {
+          // If the node has two children, we find the inorder successor
+          let successor = current.right;
+          let successorParent = current;
+  
+          // Traverse down the right subtree to find the inorder successor
+          while (successor.left) {
+            successorParent = successor;
+            successor = successor.left;
+          }
+  
+          // Replace the node to be removed with its inorder successor
+          current.val = successor.val;
+  
+          // Remove the inorder successor from the right subtree
+          if (successorParent === current) {
+            current.right = successor.right;
+          } else {
+            successorParent.left = successor.right;
+          }
+        }
+        return current;
+      }
+  
+      if (val < current.val) {
+        parent = current;
+        current = current.left;
+      } else {
+        parent = current;
+        current = current.right;
+      }
+    }
+  
+    return null;
   }
 
   /** Further Study!
